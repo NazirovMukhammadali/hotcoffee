7c68512554617285cd3774d00f09c45658df89b1
Ali <nazirovmuhammadali@MacBook-Pro-Nazirov.local>
2025-06-26 17:20:33 +0900

    Router, Controllerlar va Member - Schema Model & MIT Task - H2

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import router from "./views/router";
+import routerAdmin from "./views/routerAdmin"
 
 /** 1-ENTRACE **/
 const app = express();
@@ -14,5 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
+app.use("/admin", routerAdmin);  // BSSR: EJS
+app.use("/", router);  // SPA: REACT
 
 export default app;
--- train.ts
+++ train.ts
@@ -1,5 +1,4 @@
 // TASK G:
-
 // Yagona parametrga ega function tuzing.
 // Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
 // Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
@@ -22,19 +21,34 @@
 // console.log(getHighestIndex([10, 9, 8, 11, 6]));
 // console.log(getHighestIndex([10, 9, 8, 11, 12]));
 
+// !==============================================================================!
 
 // H-TASK: 
-
 // shunday function tuzing, u integerlardan (butun son) iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(arr: number[]) {
-    const positive = arr.filter((num) => num > 0);
-    const result = positive.join('');               // join('') stringga aylantiradi //('') single string qo'ymasam default holatda vergul qo'yadi har bir son orasiga
-    return result;
-}
+// function getPositive(arr: number[]) {
+//     const positive = arr.filter((num) => num > 0);
+// const result = positive.join('');               // join('') stringga aylantiradi //('') single string qo'ymasam default holatda vergul qo'yadi har bir son orasiga
+//     return result;
+// }
 
-console.log(getPositive([1, -4, 2]));
-console.log(getPositive([2, -4, -5, 3]));
-console.log(getPositive([2, -4, -5]));
-console.log(getPositive([-4, -5, 5]));
+// console.log(getPositive([1, -4, 2]));
+// console.log(getPositive([2, -4, -5, 3]));
+// console.log(getPositive([2, -4, -5]));
+// console.log(getPositive([-4, -5, 5]));
+
+// !==============================================================================!
+
+// H2-TASK: 
+// Shunday function tuzing, unga string argument pass bolsin.Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+function getDigits(str: string) {
+    const add = str.split("").filter(char => char >= '0' && char <= '9');
+    const result = add.join('');
+    return result;
+};
+console.log(getDigits("m14i1t"));
+console.log(getDigits("M2I7T"));
+console.log(getDigits("MI0T1"));
