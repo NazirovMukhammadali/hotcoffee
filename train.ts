55afeaa6a156eae6326271135e2764c4b0d0f10e
Ali <nazirovmuhammadali@MacBook-Pro-Nazirov.local>
2025-07-31 17:45:09 +0900

    U-Task

--- train.ts
+++ train.ts
@@ -241,10 +241,27 @@
 // Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
 // MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
-function mergeSortedArrays(arr1: number[], arr2: number[]) {
-    const add = arr1.concat(arr2);
-    add.sort((a, b) => a - b);
-    return add;
+// function mergeSortedArrays(arr1: number[], arr2: number[]) {
+//     const add = arr1.concat(arr2);
+//     add.sort((a, b) => a - b);
+//     return add;
+// }
+
+// console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+
+// U-TASK:
+// Shunday function yozing, uni number parametri bolsin va 0 dan berilgan parametrgacha bolgan oraliqdagi faqat toq sonlar nechtaligini return qilsin
+// MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
+
+function sumOdds(num: number) {
+    let count = 0;
+    for (let i = 1; i < num; i++) {
+        if (i % 2 !== 0) {
+            count++;
+        }
+    }
+    return count;
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(sumOdds(9))
+console.log(sumOdds(11))
