2ba5ee7e326981468dc6c3ed87d1c5c15cc51809
Ali <nazirovmuhammadali@MacBook-Pro-Nazirov.local>
2026-02-13 14:02:09 +0900

    fix Bugs

--- train.ts
+++ train.ts
@@ -680,11 +680,38 @@
 // Shunday function yozing, u parametridagi array ichida takrorlanmagan raqamlar yig'indisini qaytarsin.
 // MASALAN: sumOfUnique([1,2,3,2]) return 4
 
-function sumOfUnique(arr: number[]) {
-    const first = arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num))
-    const result = first.reduce((nn, num) => nn + num, 0)
-    return result;
-}
+// function sumOfUnique(arr: number[]) {
+//     const first = arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num))
+//     const result = first.reduce((nn, num) => nn + num, 0)
+//     return result;
+// }
 
-console.log(sumOfUnique([1, 2, 3, 2]));
-console.log(sumOfUnique([1, 2, 3, 2, 1, 4]));
+// console.log(sumOfUnique([1, 2, 3, 2]));
+// console.log(sumOfUnique([1, 2, 3, 2, 1, 4]));
+
+// TASK G:
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1
+
+// function getHighestIndex(arr: number) {
+
+// }
+
+// function getHighestIndex(arr: number[]) {
+//     let katta = arr[0];
+//     let indeks = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > katta) {
+//             katta = arr[i];
+//             indeks = i;
+//         }
+//     }
+//     return indeks;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8]))
+// console.log(getHighestIndex([5, 4, 12, 4, 8]));
+// console.log(getHighestIndex([10, 9, 8, 11, 6]));
+// console.log(getHighestIndex([10, 9, 8, 11, 12]));
